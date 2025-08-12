177335168a4277fa33a71ba640cb507dbbf7a97a
Whytebread <lukeweisbrot@gmail.com>
2025-08-12 10:27:24 -0500

    corrected middleware to use requireAuth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { signupUser, loginUser, getUserProfile } = require("../controllers/userController");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/requireAuth");
 
 // Public routes
 router.post("/signup", signupUser);
